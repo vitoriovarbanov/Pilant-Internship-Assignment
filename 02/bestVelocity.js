ece436101ea09b7e50fcd47d2d1f8e6459c83890
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-04-25 09:48:12 +0300

    finishing up

--- 02/bestVelocity.js
+++ 02/bestVelocity.js
@@ -1,26 +1,26 @@
-
-
-function calculateBestSprint(arr){
-    if(arr.length<3){
-        throw new Error(`test`)
+function calculateBestSprint(arr) {
+    if (arr.length < 3) {
+        throw new Error(`Sequence is too short!`)
     }
     let bestSeqArr;
     let currBestSeqSum = 0;
-    arr.forEach((el,index) => {
+    arr.forEach((firstWeekSprint, index) => {
         let secondWeekSprint = arr[index + 1]
         let thirdWeekSprint = arr[index + 2]
-        if(!secondWeekSprint || !thirdWeekSprint){
+        if (!secondWeekSprint || !thirdWeekSprint) {
             return;
         }
-        if(el + secondWeekSprint + thirdWeekSprint>=currBestSeqSum){
-            currBestSeqSum = el + secondWeekSprint + thirdWeekSprint
+        if (firstWeekSprint + secondWeekSprint + thirdWeekSprint >= currBestSeqSum) {
+            currBestSeqSum = firstWeekSprint + secondWeekSprint + thirdWeekSprint
             bestSeqArr = [];
-            bestSeqArr.push(el,secondWeekSprint,thirdWeekSprint)
+            bestSeqArr.push(firstWeekSprint, secondWeekSprint, thirdWeekSprint)
         }
     });
 
-    console.log(bestSeqArr)
-    console.log(currBestSeqSum)
+    return `{ sequence: [${bestSeqArr}], sum: ${currBestSeqSum} }`
 }
 
-calculateBestSprint([76,80,81,77,83,78,80])
+console.log(calculateBestSprint([11, 14, 10, 12]))
+console.log(calculateBestSprint([12, 9, 1, 5, 11, 5]))
+console.log(calculateBestSprint([76, 80, 81, 77, 83, 78, 80]))
+console.log(calculateBestSprint([76, 80]))
